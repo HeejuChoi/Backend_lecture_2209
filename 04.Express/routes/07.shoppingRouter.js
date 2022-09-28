af9508c855b35c04c77aff4c63f6e64d853d5537
HeejuChoi <chj12365@gmail.com>
2022-09-28 17:39:34 +0900

    Express

---
+++ 04.Express/routes/07.shoppingRouter.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const shoppingRouter = express.Router();
+
+
+shoppingRouter.get('/list', (req, res) => {
+    res.send(`<h1>http://localhost:3000/shopping/list</h1>`);
+});
+
+shoppingRouter.get('/cart', (req, res) => {
+    res.send(`<h1>http://localhost:3000/shopping/cart</h1>`);
+});
+
+module.exports = shoppingRouter; // 외부에서 볼 수 있게 라우팅 해주는 것? 
